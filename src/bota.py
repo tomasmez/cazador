acacae3fb147d695d6683be9f36caf13b326d5cd
Tomas Mezzadra <tmezzadra@gmail.com>
2024-04-24 16:02:36 -0300

    add option to change credentials via json config file
    wifi_client.json has the ssid and password for an existing
    network.
    
    if this file does not exist. we start an AP instead.

--- src/bota.py
+++ src/bota.py
@@ -4,31 +4,45 @@ import usocket as socket
 import utime
 import urequests
 from machine import Pin
-
+from cazador_del_delta import read_json_config
 # Create Access Point
 ap_ssid = "ESP32_AP"
 ap_password = ""
 utime.sleep(1)
 #ap = network.WLAN(network.AP_IF)
-sta_if = network.WLAN(network.STA_IF)
 utime.sleep(1)
 #ap.active(True)
 utime.sleep(1)
 #ap.config(essid=ap_ssid, password=ap_password)
 utime.sleep(1)
 #print('Access point started ESP32_AP')
-def do_connect():
+def do_connect(wifi_credentials):
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('thetomsons', 'Esmeralda8933')
+        sta_if.connect(wifi_credentials['ssid'], wifi_credentials['password'])
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
     
-do_connect()
+wifi_credentials=read_json_config("wifi_client.json")
+if wifi_credentials:
+    do_connect(wifi_credentials)
 
+else:
+    # Create Access Point
+    ap_ssid = "Cazador"
+    ap_password = ""
+    #utime.sleep(1)
+    ap = network.WLAN(network.AP_IF)
+    #utime.sleep(1)
+    ap.active(True)
+    #utime.sleep(1)
+    ap.config(essid=ap_ssid, password=ap_password)
+    #utime.sleep(1)
+    print(f'Access point started: SSID = {ap_ssid}, password = {ap_password}')
+    print('network config:', ap.ifconfig())
 
 #import uftpd
