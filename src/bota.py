2166c246a97106ef8ae8349f1873d9f35a264549
Luciano <luciano.moscon@gmail.com>
2024-05-07 19:21:02 -0300

    testing wifi mgr

--- src/bota.py
+++ src/bota.py
@@ -5,53 +5,19 @@ import utime
 import urequests
 from machine import Pin, RTC, freq
 from globales import read_json_config
+import wifimgr
+
 # Create Access Point
 freq(240000000)
 print(f"the frequency is {freq()}")
-ap_ssid = "ESP32_AP"
-ap_password = ""
 
-#print('Access point started ESP32_AP')
-def do_connect(wifi_credentials):
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(wifi_credentials['ssid'], wifi_credentials['password'])
 
-        for num in range(20):  # Try for 30 seconds
-            if not sta_if.isconnected():
-                print('Waiting for connection..',num,'/20 sec')
-                utime.sleep(1)
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
 
-    print('network config:', sta_if.ifconfig())
-    if sta_if.isconnected():
-        return True
-    else:
-        return False
-
-wifi_credentials=read_json_config("wifi_client.json")
-
-##############################################################
-
-if wifi_credentials:
-    wifi_connection_ok = do_connect(wifi_credentials)
-
-if not wifi_connection_ok or not wifi_connection_ok:
-    # Create Access Point
-    print(f'Starting access point..please wait')
-    ap_ssid = "Cazador"
-    ap_password = ""
-    #utime.sleep(1)
-    ap = network.WLAN(network.AP_IF)
-    #utime.sleep(1)
-    ap.active(True)
-    #utime.sleep(1)
-    ap.config(essid=ap_ssid, password=ap_password)
-    #utime.sleep(1)
-    print(f'Access point started: SSID = {ap_ssid}, password = {ap_password}')
-    print('network config:', ap.ifconfig())
 
 
 import ntptime
